a3a20143035450fe442502ea6b058bc174c68d4f
dningcheng <dningcheng@163.com>
2018-12-15 09:54:53 +0800

    commit

---
+++ seally-base/src/main/java/org/seally/base/constant/BaseConstant.java
@@ -0,0 +1,20 @@
+package org.seally.base.constant;
+/**
+ * @Date 2018年11月1日
+ * @author dnc
+ * @Description 基础常量类
+ */
+public interface BaseConstant {
+	/**角色基础 1级管理员**/
+	Integer ROLE_BASE_SUPER = 1;
+	/**角色基础 2级管理员**/
+	Integer ROLE_BASE_ADMIN = 2;
+	/**角色基础 3级管理员**/
+	Integer ROLE_BASE_MANAGER = 3;
+	/**角色基础 4级管理员**/
+	Integer ROLE_BASE_USER = 4;
+	
+	
+	/**角色基础 100=暂无角色**/
+	Integer ROLE_BASE_NONE = 100;
+}
